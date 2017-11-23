1000da7b35cc3adacc103f3f16091e230cfdd813
wwx511782 <33915034+wwx511782@users.noreply.github.com>
2017-11-23 20:44:27 +0800

    Update ServiceDelegate.java

--- src/main/java/com/service/service/controller/ServiceDelegate.java
+++ src/main/java/com/service/service/controller/ServiceDelegate.java
@@ -9,6 +9,7 @@ public class ServiceDelegate implements Service {
     public String helloworld(String name){
 
         // Do Some Magic Here!
-        return "hi, " +name + "!Welcom to ServiceStage";
+        return ''
+        //"hi, " +name + "!Welcom to ServiceStage";
     }
 }
