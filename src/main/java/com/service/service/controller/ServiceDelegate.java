6a78f6a916a4f1a36983ae770c0051106960d04e
wwx511782 <33915034+wwx511782@users.noreply.github.com>
2017-11-24 10:42:44 +0800

    Update ServiceDelegate.java

--- src/main/java/com/service/service/controller/ServiceDelegate.java
+++ src/main/java/com/service/service/controller/ServiceDelegate.java
@@ -3,12 +3,10 @@ package com.service.service.controller;
 import org.springframework.stereotype.Component;
 
 
-@Component
-public class ServiceDelegate implements Service {
-
-    public String sayHi(String name){
-
-        // Do Some Magic Here!
-        return "hi, " + name + "! welcome to ServiceStage";
-    }
-}
+@Component 
+ public class ServiceDelegate implements Service { 
+   public String helloworld(String name){ 
+   // Do Some Magic Here! 
+ return name; 
+ } 
+ }
