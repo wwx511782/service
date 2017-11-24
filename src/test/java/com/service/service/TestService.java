8ddad6757a37da2dc7de97b1e4982fbe5f8c443d
wwx511782 <33915034+wwx511782@users.noreply.github.com>
2017-11-24 10:42:02 +0800

    Update TestService.java

--- src/test/java/com/service/service/TestService.java
+++ src/test/java/com/service/service/TestService.java
@@ -6,21 +6,12 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class TestService {
-
-    ServiceDelegate serviceDelegate = new ServiceDelegate();
-
-
-    @Test
-    public void testhelloworld(){
-
-        String expactReturnValue = "hi, Tom! welcome to ServiceStage";
-            
-            //"hi, wjl!Welcom to ServiceStage"; // You should put the expect String type value here.
-
-        String returnValue = serviceDelegate.helloworld("Tom");
-
-        assertEquals(expactReturnValue, returnValue);
-    }
-
-}
+public class TestService { 
+   ServiceDelegate serviceDelegate = new ServiceDelegate(); 
+     @Test 
+ public void testhelloworld(){ 
+   String expactReturnValue = "hello"; // You should put the expect String type value here. 
+   String returnValue = serviceDelegate.helloworld("hello"); 
+   assertEquals(expactReturnValue, returnValue); 
+ } 
+   }
